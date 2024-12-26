22ee2027a33af17086614458eeadb3137d465f92
borjir <joeljayponcearcipe@gmail.com>
2024-12-26 16:45:25 +0800

    completed edit w/o finalized front end

--- item_category/urls.py
+++ item_category/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='item_categories_index'),
-    path('add/', views.add_category, name='add_category'),  # URL for the Add Category page
+    path('', views.index, name='item_categories_index'),  # View for item categories index
+    path('add/', views.add_category, name='add_category'),  # View for adding a category
+    path('delete/', views.delete_categories, name='delete_categories'),  # View for deleting categories
+    path('edit/<int:category_id>/', views.edit_category, name='edit_category'),  # Edit category page
 ]

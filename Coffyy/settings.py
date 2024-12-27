50e318763313b418c5d03bdc523fa3d8d334f3ac
borjir <joeljayponcearcipe@gmail.com>
2024-12-27 14:15:44 +0800

    settings

--- Coffyy/settings.py
+++ Coffyy/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dashboard',,
+    'dashboard',
     'item_category',
     'item_list',
 ]

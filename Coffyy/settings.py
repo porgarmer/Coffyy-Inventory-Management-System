890a52194272881f699aa1151d45a9e6777ee086
borjir <joeljayponcearcipe@gmail.com>
2024-12-27 14:22:24 +0800

    settings fixed

--- Coffyy/settings.py
+++ Coffyy/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dashboard',
     'item_category',
-    'item_list'
-]   
+    'item_list',
+] 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

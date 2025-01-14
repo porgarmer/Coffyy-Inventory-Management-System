ed68e6e0e5221cfa7ec86b2f433c107e74ac121f
noobragammer <jmtf2832@gmail.com>
2025-01-14 15:18:26 +0800

    Fixed table and redirect routes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,24 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.db.models import Q, F
 from item_list.models import Item  # Import the Item model
 from supplier.models import Supplier
 from item_category.models import Category  # Import the Category model from item_category app
 from login.models import User
+
 def index(request):
-    
-    # Calculate the number of low-stock items
-    low_stock_count = Item.objects.filter(in_stock__lte=F('reorder_level')).count()
+    if request.session['role'] == 'owner':
+        # Calculate the number of low-stock items
+        low_stock_count = Item.objects.filter(in_stock__lte=F('reorder_level')).count()
 
-    # Calculate the total number of items
-    total_items_count = Item.objects.count()
+        # Calculate the total number of items
+        total_items_count = Item.objects.count()
 
-    # Calculate the number of categories
-    category_count = Category.objects.count()
+        # Calculate the number of categories
+        category_count = Category.objects.count()
 
-    # Calculate the number of suppliers
-    supplier_count = Supplier.objects.count()
-    
-    #employee_count = User.objects.count(role = 'employee')
+        # Calculate the number of suppliers
+        supplier_count = Supplier.objects.count()
+        
+        employee_count = User.objects.filter(role='employee').count()
+    elif request.session['role'] == 'employee':
+        # Calculate the number of low-stock items
+        low_stock_count = Item.objects.filter(in_stock__lte=F('reorder_level')).count()
+
+        # Calculate the total number of items
+        total_items_count = Item.objects.count()
+
+        # Calculate the number of categories
+        category_count = Category.objects.count()
+
+        # Calculate the number of suppliers
+        supplier_count = Supplier.objects.count()
+        
+        employee_count = 0
+    else:
+        return redirect('https://www.google.com')
+        
 
     # Pass the counts to the template
     context = {
@@ -26,6 +44,6 @@ def index(request):
         'total_items_count': total_items_count,
         'category_count': category_count,
         'supplier_count': supplier_count,
-        #'employee_count': employee_count,
+        'employee_count': employee_count,
     }
     return render(request, "dashboard/dashboard.html", context)
